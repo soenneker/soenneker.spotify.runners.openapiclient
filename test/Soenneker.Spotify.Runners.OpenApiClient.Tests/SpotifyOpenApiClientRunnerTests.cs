6b06e5e68250961e9c354a172ff7812edbeb8fa2
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:39 -0500

    TUnit conversion

--- test/Soenneker.Spotify.Runners.OpenApiClient.Tests/SpotifyOpenApiClientRunnerTests.cs
+++ test/Soenneker.Spotify.Runners.OpenApiClient.Tests/SpotifyOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Spotify.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class SpotifyOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class SpotifyOpenApiClientRunnerTests : HostedUnitTest
 {
-    public SpotifyOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public SpotifyOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
